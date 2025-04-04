9a1080388df63e3b36227c45ba2042104f470995
Rosemol Shaju <93700438+Rosemolshaju@users.noreply.github.com>
2025-04-04 11:34:34 +0100

    Create Cart.vue

---
+++ src/components/Cart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>Your Cart</h3>
+    <ul>
+      <li v-for="item in cartItems" :key="item.id">
+        {{ item.product.name }} - Quantity: {{ item.quantity }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cartItems: []
+    };
+  },
+  methods: {
+    async fetchCart() {
+      const response = await fetch("http://localhost:8000/cart/?user_id=1");
+      this.cartItems = await response.json();
+    }
+  },
+  mounted() {
+    this.fetchCart();
+  }
+};
+</script>
