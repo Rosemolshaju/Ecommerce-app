0eb22842ad3c96f5ef0dcb78658abe289499dfe7
Rosemol Shaju <93700438+Rosemolshaju@users.noreply.github.com>
2025-04-04 13:34:35 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,34 +1,11 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+# models.py
+from sqlalchemy import Column, Integer, String
+from database import Base
 
 class Product(Base):
     __tablename__ = "products"
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
-    price = Column(Float)
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
-
-class CartItem(Base):
-    __tablename__ = "cart_items"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    product_id = Column(Integer, ForeignKey("products.id"))
-    quantity = Column(Integer)
-    user = relationship("User")
-    product = relationship("Product")
-
-class Order(Base):
-    __tablename__ = "orders"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    status = Column(String, default="Pending")
-    user = relationship("User")
+    price = Column(Integer)
