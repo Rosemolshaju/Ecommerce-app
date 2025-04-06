d0c0a22bfd1ecc82ecf68cca042abaeb7b6fc2d5
Rosemol Shaju <93700438+Rosemolshaju@users.noreply.github.com>
2025-04-06 14:47:58 +0400

    Update and rename tailwind.config.js to backend/login.vue

---
+++ backend/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login-form">
+    <form @submit.prevent="login">
+      <input type="text" v-model="username" placeholder="Username" />
+      <input type="password" v-model="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      // Perform login logic
+      console.log(`Logged in as ${this.username}`);
+    },
+  },
+};
+</script>
