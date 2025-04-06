a581c2faff2c3b9fd78cf7dd4902d1f208cd8995
Rosemol Shaju <93700438+Rosemolshaju@users.noreply.github.com>
2025-04-06 14:42:26 +0400

    Update and rename models.py to backend/models.py

---
+++ backend/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+    stock = Column(Integer)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
